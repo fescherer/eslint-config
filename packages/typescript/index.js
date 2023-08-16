5b286992f6a60285eba5859033be2cb0c1009920
Felipe Scherer <felipescherer25@gmail.com>
2023-08-16 19:49:59 -0300

    :sparkles: feat: typescript project

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -1,8 +1,13 @@
+const process = require('node:process')
+
+const tsconfig = process.env.ESLINT_TSCONFIG
+
+
 module.exports = {
   files: ['*.tsx', '*.jsx'],
   parserOptions: {
     tsconfigRootDir: process.cwd(),
-    project: ['./tsconfig.json'] // Precisa arrumar isso
+    project: [tsconfig]
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
