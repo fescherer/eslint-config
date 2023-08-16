58214281473d8a904c4d21147456b732aafa3018
Felipe Scherer <felipescherer25@gmail.com>
2023-08-16 12:58:44 -0300

    :sparkles: feat: all/format/lint

---
+++ packages/react/configs/rules-all.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+  extends: [
+    './rules-format',
+    './rules-lint'
+  ]
+})
---
+++ packages/typescript/configs/rules-format.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+  extends: ["@fescherer-eslint-config/base:format"],
+  rules: Object.freeze({
+  // '@typescript-eslint/comma-spacing': ['error', { before: false, after: true }]
+  '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }]
+  })
+})
